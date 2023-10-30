f060314339f11b9051be4708a40adeefc057bf01
roberttijani <roberttijani87@gmail.com>
2023-10-30 21:29:18 +0700

    setup config

--- cypress-basic/cypress.config.js
+++ cypress-basic/cypress.config.js
@@ -1,6 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportWidth:1920,
+  viewportHeight: 1080,
+  screenshotOnRunFailure: true,
+  video: false,
+  watchForFileChanges: true,
+  screenshotsFolder: "cypress/screenshots",
+  videosFolder: "cypress/videos",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
